704187c6a83509d563c7e1cdadcab5e1d9592d5a
rrobol <robolfba@gmail.com>
2025-11-30 17:15:26 -0300

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://robolfba.github.io/abocados-website/',
+  base: '/abocados-website/',
   output: 'static',
 });
